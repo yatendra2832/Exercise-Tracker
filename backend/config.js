9a3a80094c3cfc47fba872235e221de506bec801
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-01-02 23:17:15 +0530

    setup the database connection

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+const PORT = 5000;
+const MONGODBURL = 'mongodb+srv://yatendrayadav2832:yatendra@cluster0.ldyncxi.mongodb.net/exerciseapp?retryWrites=true&w=majority';
+
+module.exports = { PORT, MONGODBURL }
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
+const { PORT, MONGODBURL } = require('./config.js');
 const app = express();
 
+// cors middleware
 app.use(
     cors({
         origin: "http://localhost:5173",
@@ -15,6 +17,12 @@ app.use(
 app.use(express.json());
 
 // Server listening
-app.listen(3000, () => {
-    console.log('Server is Running at port 3000');
-})
+mongoose
+    .connect(MONGODBURL)
+    .then(() => {
+        console.log('Connected to Database Successfully');
+        app.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`);
+        })
+    })
+    .catch((err) => console.log("Error in Connecting to Database: ", err));
