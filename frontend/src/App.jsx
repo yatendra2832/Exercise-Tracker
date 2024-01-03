d6a3a3746146783b8c1feeadf0853e028af7e715
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-01-03 22:27:14 +0530

    Create Routes for all the components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,22 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+// all the imports of the page
+import Navbar from "./components/Navbar";
+import ExerciseList from "./components/ExerciseList";
+import EditExercise from "./components/EditExercise";
+import CreateExercise from "./components/CreateExercise";
+import User from "./components/User";
 function App() {
   return (
-    <div className="container">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <br />
+      <Route path="/" element={<ExerciseList />} />
+      <Route path="/edit/:id" element={<EditExercise />} />
+      <Route path="/create" element={<CreateExercise />} />
+      <Route path="/user" element={<User />} />
+    </Router>
   );
 }
 
