7f5123e6a44c3581431022da79337785b69be985
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-01-04 13:40:51 +0530

    add the username selection working by fetching username from database

--- frontend/src/components/CreateExercise.jsx
+++ frontend/src/components/CreateExercise.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-
+import axios from "axios";
 const CreateExercise = () => {
   const [exercise, setExercise] = useState({
     username: "",
@@ -11,10 +11,38 @@ const CreateExercise = () => {
     users: [],
   });
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/users");
+        // console.log(response.data[0].username);
+        if (response.data.length > 0) {
+          setExercise((prevExercise) => ({
+            ...prevExercise,
+            users: response.data.map((user) => user.username),
+            username: response.data[0].username,
+          }));
+        }
+      } catch (error) {
+        console.log("Error in Fetching Data:", error.message);
+      }
+    };
+
+    fetchData();
+  }, []); // Dependency array is empty, meaning it runs once after the initial render
+
   const handleInput = (e) => {
     const name = e.target.name;
     const value = e.target.value;
     setExercise({ ...exercise, [name]: value });
+  };
+
+  const handleUsernameChange = (e) => {
+    const selectedUsername = e.target.value;
+    setExercise((prevExercise) => ({
+      ...prevExercise,
+      username: selectedUsername,
+    }));
   };
 
   const handleDateChange = (date) => {
@@ -24,18 +52,27 @@ const CreateExercise = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     console.log(exercise);
+    axios
+      .post("http://localhost:5000/exercise/add", exercise)
+      .then(() => {
+        console.log(exercise);
+        alert("Exercise added Successfully");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
   };
   return (
     <div className="container">
       <h3>Create New Exercise Log</h3>
       <form onSubmit={onSubmit}>
-        <div className="form-group">
+        <div className="form-group mb-3">
           <label htmlFor="username">Username</label>
           <select
             required
             className="form-control"
             value={exercise.username}
-            onChange={handleInput}
+            onChange={handleUsernameChange}
           >
             {exercise.users.map((user) => {
               return (
@@ -46,7 +83,7 @@ const CreateExercise = () => {
             })}
           </select>
         </div>
-        <div className="form-group">
+        <div className="form-group mb-3">
           <label htmlFor="description">Description</label>
           <input
             type="text"
@@ -59,7 +96,7 @@ const CreateExercise = () => {
             onChange={handleInput}
           />
         </div>
-        <div className="form-group">
+        <div className="form-group mb-3">
           <label htmlFor="duration">Duration (in min)</label>
           <input
             type="number"
@@ -71,7 +108,7 @@ const CreateExercise = () => {
             onChange={handleInput}
           />
         </div>
-        <div className="form-group">
+        <div className="form-group mb-3">
           <label htmlFor="date">Date</label>
           <div>
             <DatePicker
@@ -83,7 +120,7 @@ const CreateExercise = () => {
             />
           </div>
         </div>
-        <div className="form-group">
+        <div className="form-group mb-3">
           <input
             type="submit"
             value="Create Exercise Log"
